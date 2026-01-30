618380dd1f0ffa81871d3416105d037f8897a8e0
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-01-30 22:30:59 +0100

    Add files via upload

---
+++ YP/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_1_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Multireligionvalsystem.",
+ image: "FDU_htm_files/1409.png"
+},
+{
+ caption: "political practices in exile ",
+ image: "FDU_htm_files/1410.png"
+},
+{
+ caption: "F\u00F6r en h\u00E5llbar politik som leder  till en h\u00E5llbar utveckling. ",
+ image: "FDU_htm_files/1411.png"
+},
+{
+ caption: "Den 77 \u00E5r l\u00E5nga konflikten mellan Israel och Palestina\u000A.",
+ image: "FDU_htm_files/1412.png"
+}
+];
